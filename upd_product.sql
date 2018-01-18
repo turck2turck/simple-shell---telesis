bf8fde0d280a55db2845d5919d8687220860eea1
Ubuntu <ubuntu@ip-10-0-20-247.ec2.internal>
2018-01-18 22:14:49 +0000

    Initial add to repository.

---
+++ upd_product.sql
@@ -0,0 +1,4 @@
+UPDATE public.product p
+SET cat_sub_assoc_id=e.atero_cat_id, type='Product', name=e.name, depth=e.length, height=e.height, width=e.width, shipping_weight=e.shipping_weight, tax_code=e.avalara_tax_code, freight_class=e.freight_class, freight_code=e.avalara_freight_code, description=e.key_features, msrp=e.price_usd, utility_info=e.key_utility_information, sku=e.sku, updated_at=current_timestamp, updated_by=2
+FROM loading.akeneo_error e
+WHERE e.error_msg = 'Update' and e.product_hash = p.product_hash;
